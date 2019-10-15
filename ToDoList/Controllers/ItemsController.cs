6bf10b984e85ca25fe91ad8362a139d810e1e378
Student <example@example.com>
2019-10-15 14:58:55 -0700

    Add edit button for user to edit their entry

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,43 +2,57 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller
-  {
-    private readonly ToDoListContext _db;
-
-    public ItemsController(ToDoListContext db)
+    public class ItemsController : Controller
     {
-      _db = db;
-    }
+        private readonly ToDoListContext _db;
 
-    public ActionResult Index()
-    {
-      List<Item> model = _db.Items.ToList();
-      return View(model);
-    }
+        public ItemsController(ToDoListContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Item> model = _db.Items.ToList();
+            return View(model);
+        }
 
 
-    public ActionResult Create()
-    {
-        return View();
-    }
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-    [HttpPost]
-    public ActionResult Create(Item item)
-    {
-        _db.Items.Add(item);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-    }
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-    public ActionResult Details(int id)
-    {
-        Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-        //^This is our way of communicating to the database, giving it the id that was passed in and telling the database to give us the item in the database that has this id. In this line, "items" is not a special name.
-        return View(thisItem);
+        public ActionResult Details(int id)
+        {
+            Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+            //^This is our way of communicating to the database, giving it the id that was passed in and telling the database to give us the item in the database that has this id. In this line, "items" is not a special name.
+            return View(thisItem);
+        }
+        public ActionResult Edit(int id)
+        {
+            var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Item item)
+        {
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
